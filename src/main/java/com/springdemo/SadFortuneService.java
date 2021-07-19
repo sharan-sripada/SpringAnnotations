cc46c900ae4f18ba547f5019d5663c6620cd5a6d
sharan-sripada <sharan.sripada@zemosolabs.com>
2021-07-19 21:14:18 +0530

    added

---
+++ src/main/java/com/springdemo/SadFortuneService.java
@@ -0,0 +1,13 @@
+package com.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SadFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return "Today is a sad day";
+    }
+
+}
